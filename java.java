b18fa2beff22119fb1a5eb74f6955c5b2f50c210
Firly Aulia Azzahra ( 20090064) <80614009+firlyauliaazzahra@users.noreply.github.com>
2021-05-26 22:50:56 +0700

    Add files via upload

---
+++ java.java
@@ -0,0 +1,21 @@
+package singlylinkedlistApp;
+ public class java {
+    public static void main(String[] ar) {
+        SinglyLinkedList newList = new SinglyLinkedList();
+        newList.insertAwal(100);
+        newList.display();
+        newList.insertAkhir(200);
+        newList.display();
+        newList.insertAwal(50);
+        newList.display();
+        newList.insertAkhir(80, 2);
+        newList.display();
+        newList.DeleteAtPos(1);
+        newList.display();
+    }
+ }
+
+            
+        
+    
+
